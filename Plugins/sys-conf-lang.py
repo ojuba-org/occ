7c45270b0b6c7dd8d689d5e00f5a7dfe7bf3b4ee
Ehab El-Gedawy <ehabsas@gmail.com>
2014-03-16 03:14:14 +0200

    fix system config language plugin

--- Plugins/sys-conf-lang.py
+++ Plugins/sys-conf-lang.py
@@ -19,7 +19,8 @@ Copyright © 2009, Ojuba Team <core@ojuba.org>
 """
 
 from gi.repository import Gtk
-import re
+import pwd, os
+import ConfigParser
 from OjubaControlCenter.widgets import LaunchOrInstall,info,error
 from OjubaControlCenter.pluginsClass import PluginsClass
 
@@ -29,41 +30,62 @@ caption = _('System Language:')
 description = ''
 priority = 10
 
+## TODO: add more languages, add gdm language
 class occPlugin(PluginsClass):
-    lang_re=re.compile(r'''^(\s*LANG\s*=\s*)['"]([^"']*)['"]''')
-    lang_fn='/etc/sysconfig/i18n'
+    conf = ConfigParser.ConfigParser()
+    u = pwd.getpwuid(os.geteuid())[0]
+    lang_fn = '/var/lib/AccountsService/users/%s' %u
     def __init__(self,ccw):
         PluginsClass.__init__(self, ccw, caption, category, priority)
-        vb=Gtk.VBox(False,2)
+        vb = Gtk.VBox(False,2)
         self.add(vb)
-        h=Gtk.HBox(False,2); vb.pack_start(h,False,False,6)
-        l=Gtk.Label(_('Select the default system language'))
+        h = Gtk.HBox(False,2); vb.pack_start(h,False,False,6)
+        l = Gtk.Label(_('Select the default system language'))
         h.pack_start(l,False,False,2)
-        h=Gtk.HBox(False,2); vb.pack_start(h,False,False,6)
+        h = Gtk.HBox(False,2); vb.pack_start(h,False,False,6)
         self.ar_b = b = Gtk.Button("العربية")
-        b.connect('clicked', self.set_lang, "ar_EG.UTF-8")
+        b.connect('clicked', self.set_lang, "ar_EG.utf8")
+        h.pack_start(b,False,False,2)
+        self.fr_b = b = Gtk.Button("Frensh")
+        b.connect('clicked', self.set_lang, "fr_FR.utf8")
         h.pack_start(b,False,False,2)
         self.en_b = b = Gtk.Button("English")
         b.connect('clicked', self.set_lang)
         h.pack_start(b,False,False,2)
-        h.pack_start(LaunchOrInstall(self,_('Other Language'),'/usr/bin/system-config-language',['system-config-language']),False,False,0)
+        #h.pack_start(LaunchOrInstall(self, _('Other Language'), lang_fn, ['system-config-language']), False, False, 0)
         self.get_lang()
 
     def get_lang(self):
-        try: lang=open(self.lang_fn, 'r').read().strip()
-        except OSError: lang=""
-        m=self.lang_re.search(lang)
-        if m:
-            self.ar_b.set_sensitive(not m.group(2).startswith('ar_'))
-            self.en_b.set_sensitive(not m.group(2).startswith('en_'))
+        self.ar_b.set_sensitive(False)
+        self.fr_b.set_sensitive(False)
+        self.en_b.set_sensitive(False)
+        
+        lang = None
+        
+        self.conf.read(self.lang_fn)
+        
+        if self.conf.has_option("User", "Language"):
+            lang = self.conf.get("User", "Language")
+        
+        if lang:
+            self.ar_b.set_sensitive(not lang.startswith('ar_'))
+            self.fr_b.set_sensitive(not lang.startswith('fr_'))
+            self.en_b.set_sensitive(not lang.startswith('en_'))
+        
         return lang
 
-    def set_lang(self, w, nl="en_US.UTF-8"):
-        lang=self.get_lang()
-        l,e=self.lang_re.subn(r'\1"%s"' %nl,lang)
-        #print l,nl
-        if e==0: l='''LANG="en_US.UTF-8"\nSYSFONT="latarcyrheb-sun16"'''
-        self.ccw.mechanism('run','write_conf',self.lang_fn, l)
-        self.get_lang()
-        
+    def set_lang(self, w, nl="en_US.utf8"):
+        ## setup  new config if config file not exist
+        config = [('language', nl), ('xsession', 'gnome'), ('systemaccount', 'false')]
+        ## change language if config file exist
+        if self.conf.has_option("User", "Language"):
+            self.conf.set("User", "Language", nl)
+            config = self.conf.items("User")
+            
+        c_list = map(lambda (a, b):"%s=%s" %(a,b), config)
+        c_text = "[User]\n%s\n" % "\n".join(c_list)
 
+        self.ccw.mechanism('run','write_conf',self.lang_fn, c_text)
+        return self.get_lang()
+        
+    
