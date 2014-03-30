dd6501c1dfa70ceb9f58f21d36b130d3f036de63
Ehab El-Gedawy <ehabsas@gmail.com>
2014-03-30 16:07:07 +0200

    load icons using plugin name

--- OjubaControlCenter/mainGUI.py
+++ OjubaControlCenter/mainGUI.py
@@ -176,7 +176,9 @@ class GUI(Gtk.VBox):
         # we must remove char : in the caption ?
         caption = plugin['caption'].replace(':', '')
         # FIXME: make fixed size main button widget
-        icon = os.path.join(self.__iconsDir,  '%s.png' % caption )
+        icon = os.path.join(self.__iconsDir,  '%(name)s.png' % plugin )
+        if not os.path.isfile(icon):
+            icon = os.path.join(self.__iconsDir,  '%(name)s.svg' % plugin )
         if os.path.isfile(icon):
             b = MainButton(caption, img_fn=icon)
         else:
